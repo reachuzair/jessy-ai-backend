13701283766343c01eece518c2b721beba3ac76a
Jeremy Sigamony <jeremy.sigamony@ccript.com>
2025-08-27 01:02:32 +0500

    h

--- jessy-python-backend/run.py
+++ jessy-python-backend/run.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 if __name__ == "__main__":
     uvicorn.run(
         "src.app:app",
-        host="0.0.0.0",
+        host="localhost",
         port=8003,
         reload=True,
         log_level="info"
