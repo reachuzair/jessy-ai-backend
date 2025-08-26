72f6ea4d0697fda03468e58bfdfd1882859ec885
Jeremy Sigamony <jeremy.sigamony@ccript.com>
2025-08-26 21:24:46 +0500

    Update run.py to change host to localhost and port to 8003

--- jessy-python-backend/run.py
+++ jessy-python-backend/run.py
@@ -14,8 +14,8 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 if __name__ == "__main__":
     uvicorn.run(
         "src.app:app",
-        host="0.0.0.0",
-        port=8000,
+        host="localhost",
+        port=8003,
         reload=True,
         log_level="info"
     )
